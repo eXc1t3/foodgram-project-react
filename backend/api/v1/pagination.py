6f8f31fce8201d8b0d6ec95780865afb74768b16
Orlan Uzum <uzumorlan@mail.ru>
2024-04-13 20:42:51 +0300

    123

--- backend/api/v1/pagination.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-from foodgram_backend.constants import PAGE_SIZE
-
-
-class PageLimitPagination(PageNumberPagination):
-    """Пагинация"""
-    page_size_query_param = 'limit'
-    page_size = PAGE_SIZE
