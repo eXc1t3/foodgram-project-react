667d743029f1cb03c4a86de0f2b856b41eb27dd8
Orlan Uzum <uzumorlan@mail.ru>
2024-04-13 15:17:35 +0300

    fix 31

--- backend/recipes/managemant/commands/load_csv.py
+++ backend/recipes/managemant/commands/load_csv.py
@@ -1,22 +1,16 @@
 import csv
-
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
 from recipes.models import Ingredient, Tag
 
-
 class Command(BaseCommand):
+    help = 'Загрузка из csv файла'
 
     def import_ingredient(self):
         if Ingredient.objects.all().exists():
             print('Данные ингредиентов уже загружены')
         else:
-            with open(settings.BASE_DIR / 'data/ingredients.csv',
-                'r',
-                encoding='utf8',
-                newline=''
-            ) as file:
+            with open(settings.BASE_DIR / 'data/ingredients.csv', 'r', encoding='utf8', newline='') as file:
                 reader = csv.DictReader(file)
                 for row in reader:
                     Ingredient.objects.create(
@@ -28,16 +22,10 @@ class Command(BaseCommand):
         if Tag.objects.all().exists():
             print('Данные тэгов уже загружены')
         else:
-            with open(
-                settings.BASE_DIR / 'data/tags.csv',
-                'r',
-                encoding='utf8',
-                newline=''
-            ) as file:
+            with open(settings.BASE_DIR / 'data/tags.csv', 'r', encoding='utf8', newline='') as file:
                 reader = csv.DictReader(file)
                 for row in reader:
-                    Tag.objects.create(name=row['name'], color=row['color'],
-                                       slug=row['slug'])
+                    Tag.objects.create(name=row['name'], color=row['color'], slug=row['slug'])
                 print('Данные тэгов успешно загружены')
 
     def handle(self, *args, **kwargs):
