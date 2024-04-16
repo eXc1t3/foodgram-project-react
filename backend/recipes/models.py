288b2f6a458253ca61c5c7a7d67d2b78053f9ff0
Orlan Uzum <uzumorlan@mail.ru>
2024-04-16 14:24:25 +0300

    123

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -90,14 +90,14 @@ class Recipe(models.Model):
         Tag,
         related_name='recipes',
         verbose_name='Tags')
-    favorites = models.ManyToManyField(
+    favorites = models.ManyToManyField( 
         User,
-        related_name='favorites_recipes',
+        related_name='favorites',
         verbose_name='Избранное',
         blank=True)
     shopping_cart = models.ManyToManyField(
         User,
-        related_name='shopping_cart_recipes',
+        related_name='shopping_cart',
         verbose_name='Список покупок',
         blank=True)
     created = models.DateTimeField(
