4a2d89075bd829a0bfdb5f6798e81d3d5aee4e8e
Orlan Uzum <uzumorlan@mail.ru>
2024-04-16 16:12:19 +0300

    123

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -290,7 +290,6 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         recipe = Recipe.objects.create(**validated_data)
         recipe.tags.set(tags)
         self.add_ingredients(recipe, ingredients)
-        recipe.save()
         return recipe
 
     def update(self, instance, validated_data):
@@ -300,7 +299,6 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         instance.ingredients.clear()
         instance.tags.set(tags)
         self.add_ingredients(instance, ingredients)
-        instance.save()
         return instance
 
 
