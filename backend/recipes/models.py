86146347df8da96bcaaef2d15463eda4914ebc0b
Orlan Uzum <uzumorlan@mail.ru>
2024-04-16 13:37:17 +0300

    123

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -88,7 +88,7 @@ class Recipe(models.Model):
         verbose_name='Ингредиенты')
     tags = models.ManyToManyField(
         Tag,
-        related_name='tags',
+        related_name='recipes',
         verbose_name='Tags')
     favorites = models.ManyToManyField(
         User,
