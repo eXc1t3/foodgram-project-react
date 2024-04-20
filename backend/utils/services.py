12e4a678b7f577d80481c480c89c4387ad4020f8
Orlan Uzum <uzumorlan@mail.ru>
2024-04-20 15:16:46 +0300

    123

--- backend/utils/services.py
+++ backend/utils/services.py
@@ -59,11 +59,11 @@ def add_or_del_obj(pk, request, param, serializer_context):
     """Функция для добавления или удаления объекта."""
 
     obj = get_object_or_404(Recipe, pk=pk)
-    if request.method == 'DELETE':
-        if param.filter(pk=obj.pk).exists():
-            param.remove(obj)
-            return Response(status=status.HTTP_204_NO_CONTENT)
-    elif request.method == 'POST' and not param.filter(pk=obj.pk).exists():
+    obj_bool = param.filter(pk=obj.pk).exists()
+    if request.method == 'DELETE' and obj_bool:
+        param.filter(pk=obj.pk).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    if request.method == 'POST' and not obj_bool:
         param.add(obj)
         serializer = serializer_context(
             obj, context={'request': request})
