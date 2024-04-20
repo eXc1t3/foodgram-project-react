8e96ee1027df9b39a05cef1901a762cf6436a988
Orlan Uzum <uzumorlan@mail.ru>
2024-04-20 15:38:10 +0300

    123

--- backend/utils/services.py
+++ backend/utils/services.py
@@ -1,14 +1,11 @@
 import io
 
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from rest_framework import status
 from rest_framework.response import Response
-
-from recipes.models import Recipe
 
 from .constans import (FONT_HEIGHT, HORISONTAL_POSITION_TEXT_ON_PAGE,
                        HORISONTAL_POSITION_TITUL_ON_PAGE, MAX_INTERVAL_LINES,
@@ -58,9 +55,4 @@ def create_shopping_cart(ingredients_cart):
 def add_or_del_obj(pk, request, param, serializer_context):
     """Функция для добавления или удаления объекта."""
 
-    obj = get_object_or_404(Recipe, pk=pk)
-    obj_bool = param.filter(pk=obj.pk).exists()
-    if request.method == 'DELETE' and obj_bool:
-        param.filter(pk=obj.pk).delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
     return Response(status=status.HTTP_400_BAD_REQUEST)
